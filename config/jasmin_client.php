9776e011c0c456d31fb2d33137379ef86b4b10ad
David Ringle <ringunger@gmail.com>
2024-09-12 17:30:10 +0300

    Initial Commit

---
+++ config/jasmin_client.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'jasmin_url' => env('JASMIN_URL'),
+    'jasmin_username' => env('JASMIN_USERNAME'),
+    'jasmin_password' => env('JASMIN_PASSWORD'),
+];
---
+++ src/Facades/JasminClient.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace src\Facades;
+
+class JasminClient
+{
+
+}
