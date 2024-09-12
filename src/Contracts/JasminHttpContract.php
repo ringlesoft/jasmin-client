a2e685e9d9ca5be05395b542b37d60ecf8a5fd6a
David Ringle <ringunger@gmail.com>
2024-09-12 21:44:16 +0300

    HTTP (WIP)

--- src/Contracts/JasminHttpContract.php
+++ src/Contracts/JasminHttpContract.php
@@ -5,8 +5,8 @@ namespace RingleSoft\JasminClient\Contracts;
 interface JasminHttpContract
 {
 
-    // Authenticate
     // Send a single message
+    public function sendMessage(string $content, string $to, string $from, string $coding, int $priority, string $sdt, string $validityPeriod, string $dlr, string $dlrUrl, string $dlrLevel, string $dlrMethod, ?string $tags, ?string $hexContent,  ?bool $asBinary = false): JasminHttpResponse;
     // Send multiple messages
     // Send binary messages
     // Balance check
--- src/Services/HttpService.php
+++ src/Services/HttpService.php
@@ -7,6 +7,17 @@ use RingleSoft\JasminClient\Models\JasminHttpResponse;
 
 class HttpService implements JasminHttpContract
 {
+    private string $url;
+    private string $username;
+    private string $password;
+    public function __construct(?string $username, ?string $password, ?string $url)
+    {
+        $this->url = $url ?? config('jasmin_client.url');
+        $this->username = $username ?? config('jasmin_client.username');
+        $this->password = $password ?? config('jasmin_client.password');
+    }
+
+
     public function sendMessage(string $content, string $to, string $from, string $dlr, string $dlrUrl, string $dlrLevel): JasminHttpResponse
     {
         return '';
