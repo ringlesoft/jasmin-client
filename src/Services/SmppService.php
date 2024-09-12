697f0ac13982111493de0729b70655ae72c78989
David Ringle <ringunger@gmail.com>
2024-09-12 21:44:32 +0300

    SMPP (WIP)

--- src/Services/SmppService.php
+++ src/Services/SmppService.php
@@ -7,4 +7,14 @@ use RingleSoft\JasminClient\Contracts\JasminHttpContract;
 class SmppService implements JasminHttpContract
 {
 
+    private string $url;
+    private string $username;
+    private string $password;
+    public function __construct(?string $username, ?string $password, ?string $url)
+    {
+        $this->url = $url ?? config('jasmin_client.url');
+        $this->username = $username ?? config('jasmin_client.username');
+        $this->password = $password ?? config('jasmin_client.password');
+    }
+
 }
