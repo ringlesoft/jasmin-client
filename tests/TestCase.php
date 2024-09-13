be3f4ee38003acbf00f732cd5af84d07a5fff76a
David Ringle <ringunger@gmail.com>
2024-09-13 16:02:19 +0300

    Test Environment

---
+++ tests/TestCase.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace RingleSoft\JasminClient\Tests;
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+class TestCase extends BaseTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+}
