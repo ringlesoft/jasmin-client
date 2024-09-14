abb284f9a2e2f5f5b249ba7b343720c61ee5d38e
David Ringle <ringunger@gmail.com>
2024-09-14 14:23:07 +0300

    Going away from multiple Jasmin Response classes

--- src/Models/Responses/JasminHttpResponse.php
+++ src/Models/Responses/JasminHttpResponse.php
@@ -6,13 +6,4 @@ use Illuminate\Http\Client\Response;
 
 class JasminHttpResponse extends JasminResponse
 {
-
-    public static function from(Response $response): JasminHttpResponse
-    {
-        $self = new self($response->status(), $response->body(), $response->json());
-        $self->message = $response->body();
-        $self->data = $response->json();
-        $self->body = $response->body();
-        return $self;
-    }
 }
--- src/Models/Responses/JasminRestResponse.php
+++ src/Models/Responses/JasminRestResponse.php
@@ -6,11 +6,4 @@ use Illuminate\Http\Client\Response;
 
 class JasminRestResponse extends JasminResponse
 {
-
-    public static function from(Response $response): self
-    {
-        $self = new self($response->status(), $response->body(), $response->json());
-        $self->body = $response->body();
-        return $self;
-    }
 }
