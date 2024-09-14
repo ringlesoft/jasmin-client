15fac26d45df4962fd6d73a14efc7e5912019b7c
David Ringle <ringunger@gmail.com>
2024-09-14 14:21:53 +0300

    Adjusted a few config variables

--- config/jasmin_client.php
+++ config/jasmin_client.php
@@ -3,7 +3,7 @@ return [
     'url' => env('JASMIN_BASE_URL', 'http://127.0.0.1:1404'),
     'username' => env('JASMIN_USERNAME', "jasminadmin"),
     'password' => env('JASMIN_PASSWORD', "jasminpwd"),
-    'default_callback_url' => env('JASMIN_CALLBACK_URL', 'http://localhost/jasmin-callback'),
+    'dlr_callback_url' => env('JASMIN_DLR_CALLBACK_URL', 'http://localhost/jasmin-callback'),
     'batch_callback_url' => env('JASMIN_BATCH_CALLBACK_URL', 'http://localhost/jasmin-batch-callback'),
     'batch_errback_url' => env('JASMIN_BATCH_ERRBACK_URL', 'http://localhost/jasmin-batch-callback'),
 
